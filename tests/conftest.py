53d521fe02389df75ed2cefda7fc04b9d6153ff0
syedjahangir786 <syedjahangir301996@gmail.com>
2024-12-23 15:10:52 +0530

    updated conftest for edge

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,7 @@ import pytz
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--browser", action="store", default="edge")
 
 
 @pytest.fixture(scope='class')
